3e3a59a2cc70b62003c5807358d2caafd895efc0
javohirhasano <ijavoxir5@gmail.com>
2021-09-18 00:18:21 +0500

    'tiozgardi'

--- src/App.js
+++ src/App.js
@@ -29,20 +29,20 @@ export default function App() {
                 <button className="clers main" id="clear" onClick={clear}>Clear</button>
                 <button className="clers" id="backspace" onClick={backspace}>C</button>
                 <button className="clers" name="." onClick={handleClick}>.</button>
-                <button name="7" onClick={handleClick}>9</button>
+                <button name="9" onClick={handleClick}>9</button>
                 <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>7</button>
-                <button name="4" onClick={handleClick}>6</button>
+                <button name="7" onClick={handleClick}>7</button>
+                <button name="6" onClick={handleClick}>6</button>
                 <button className="clers" name="*" onClick={handleClick}>&times;</button>
                 <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>4</button>
+                <button name="4" onClick={handleClick}>4</button>
                 <button name="3" onClick={handleClick}>3</button>
                 <button className="clers" name="-" onClick={handleClick}>&ndash;</button>
                 <button name="2" onClick={handleClick}>2</button>
                 <button name="1" onClick={handleClick}>1</button>
-                <button name="1" onClick={handleClick}>0</button>
+                <button name="0" onClick={handleClick}>0</button>
                 <button className="clers" name="/" onClick={handleClick}>&divide;</button>
-                <button className="clers" name="." onClick={handleClick}>+</button>
+                <button className="clers" name="+" onClick={handleClick}>+</button>
                 <button className="clers teng" name="=" onClick={calculate}>=</button>
             </div>
         </div>
