6a0630773d180bba349e3b3bb18a008979e79108
javohirhasano <ijavoxir5@gmail.com>
2021-09-18 00:11:49 +0500

    'uzgardi'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,14 @@ export default function App() {
         setResult("")
     }
     const backspace = () => {
-        setResult(result.slice(0, result.length - 1))
+        setResult(result.slice(0, -1))
+    }
+    const calculate = () => {
+        try {
+            setResult(eval(result).toString())
+        } catch (Error) {
+            setResult("Error")
+        }
     }
     return (
         <div className="container">
@@ -18,22 +25,25 @@ export default function App() {
             </form>
 
             <div className="keypad">
-                <button name="7" onClick={handleClick}>7</button>
-                <button className="clers" id="clear" onClick={clear}>Clear</button>
+
+                <button className="clers main" id="clear" onClick={clear}>Clear</button>
                 <button className="clers" id="backspace" onClick={backspace}>C</button>
-                <button className="clers" name="/" onClick={handleClick}>&divide;</button>
+                <button className="clers" name="." onClick={handleClick}>.</button>
+                <button name="7" onClick={handleClick}>9</button>
                 <button name="8" onClick={handleClick}>8</button>
-                <button name="9" onClick={handleClick}>9</button>
-                <button name="4" onClick={handleClick}>4</button>
+                <button name="9" onClick={handleClick}>7</button>
+                <button name="4" onClick={handleClick}>6</button>
                 <button className="clers" name="*" onClick={handleClick}>&times;</button>
                 <button name="5" onClick={handleClick}>5</button>
-                <button name="6" onClick={handleClick}>6</button>
-                <button name="1" onClick={handleClick}>1</button>
+                <button name="6" onClick={handleClick}>4</button>
+                <button name="3" onClick={handleClick}>3</button>
                 <button className="clers" name="-" onClick={handleClick}>&ndash;</button>
                 <button name="2" onClick={handleClick}>2</button>
-                <button name="3" onClick={handleClick}>3</button>
-                <button className="clers" name="." onClick={handleClick}>.</button>
-                <button className="clers" name="=" onClick={handleClick}>=</button>
+                <button name="1" onClick={handleClick}>1</button>
+                <button name="1" onClick={handleClick}>0</button>
+                <button className="clers" name="/" onClick={handleClick}>&divide;</button>
+                <button className="clers" name="." onClick={handleClick}>+</button>
+                <button className="clers teng" name="=" onClick={calculate}>=</button>
             </div>
         </div>
     )
